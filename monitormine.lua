97cb8ff0ca4ce1e3c6b3625fdf804f8d7662627e
Piguman <113848275+piguman3@users.noreply.github.com>
2022-09-18 16:48:05 -0300

    Add files via upload

--- monitormine.lua
+++ monitormine.lua
@@ -4,6 +4,8 @@ rednet = peripheral.find("modem", rednet.open)
 local turtleAmount
 local turtles = {}
 
+local mined = 0
+
 monitor.clear()
 monitor.setTextScale(0.5)
 monitor.setCursorPos(1,1)
@@ -33,7 +35,7 @@ function askForTurtles()
     local yn = io.read()
 
     if yn=="Y" then
-        print("Please put all turtles next to the PC one on top of each other and press ENTER.")
+        print("Please put all turtles one on top of each other and press ENTER.")
         io.read()
     elseif yn=="N" then
         askForTurtles()
@@ -63,11 +65,16 @@ local d = io.read()
 print("Height: (Down, moves entire column.)")
 local h = io.read()
 
+local needMine=w*d*h+w/2
+
 print("Turtle calling finished. Starting mining process. \n")
 
 function sendCom(turtle, command) --Sends a string to turtle
     rednet.send(turtle, command)
     local id, message = rednet.receive(nil, 5)
+    if command=="DIG" then
+        mined=mined+1
+    end
     monitor.clearLine()
     print("Cleared")
     if not id then
@@ -81,10 +88,12 @@ end
 function sendComAll(command)
     monitor.setCursorPos(1,1)
     monitor.write("Current status of mining process")
-    monitor.setCursorPos(1,2)
+    monitor.setCursorPos(1,3)
     monitor.write("--------------------------------")
     for x=1,#turtles,1 do --Loop for all turtles
-        monitor.setCursorPos(1,x+3)
+        monitor.setCursorPos(1,2)
+        monitor.write(math.floor(mined/needMine*100) .. "% Done        ")
+        monitor.setCursorPos(1,x+4)
         monitor.write("1 \2 - State: " .. sendCom(turtles[x], command)) --Prints results from sendCom
     end
 end
@@ -147,6 +156,8 @@ end
 for x=1,turtleAmount*h,1 do
     sendComAll("DWN")
 end
+
+sleep(0.5)
 
 monitor.clear()
 monitor.setCursorPos(1,1)
