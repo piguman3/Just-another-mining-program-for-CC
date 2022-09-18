c1a4916d2a57e2c778af6081014947e5ee8215ee
Piguman <113848275+piguman3@users.noreply.github.com>
2022-09-18 13:01:02 -0300

    Add files via upload

---
+++ monitormine.lua
@@ -0,0 +1,158 @@
+local monitor = peripheral.wrap("left")
+rednet = peripheral.find("modem", rednet.open)
+
+local turtleAmount
+local turtles = {}
+
+monitor.clear()
+monitor.setTextScale(0.5)
+monitor.setCursorPos(1,1)
+monitor.write("Please insert IDs of turtles in PC")
+
+term.clear()
+term.setCursorPos(1,1)
+print("Install the turtle program on all turtles that are going to be used.")
+function askForTurtles()
+    print("Number of turtles:")
+    turtleAmount = io.read()
+    turtles = {}
+
+    for x=1,turtleAmount,1 do 
+        print(x .. " Turtle ID:")
+        local id = tonumber(io.read())
+        turtles[x] = id
+        print("ID of turtle " .. x .. " is " .. id)
+    end
+
+    print("So the turtle ids are:")
+    for x=1,#turtles,1 do 
+        print(turtles[x])
+    end
+
+    print("Is this correct? [Y / N]")
+    local yn = io.read()
+
+    if yn=="Y" then
+        print("Please put all turtles next to the PC one on top of each other and press ENTER.")
+        io.read()
+    elseif yn=="N" then
+        askForTurtles()
+    end
+end
+
+askForTurtles()
+
+print("Calling turtles...")
+monitor.clear()
+monitor.setCursorPos(1,1)
+monitor.write("Calling turtles...")
+for x=1,#turtles,1 do 
+    rednet.send(turtles[x], "This is computer " .. os.getComputerID())
+    local id, message = rednet.receive(nil, 5)
+    if not id then
+        printError("No message received")
+    else
+        print(("Turtle %d sent message: %s"):format(id, message))
+    end
+end
+
+print("Width:")
+local w = io.read()
+print("Depth:")
+local d = io.read()
+print("Height: (Down, moves entire column.)")
+local h = io.read()
+
+print("Turtle calling finished. Starting mining process. \n")
+
+function sendCom(turtle, command) --Sends a string to turtle
+    rednet.send(turtle, command)
+    local id, message = rednet.receive(nil, 5)
+    monitor.clearLine()
+    print("Cleared")
+    if not id then
+        printError("No message received")
+    else
+        print(message)
+        return tostring(message) --Outputs state of turtle
+    end
+end
+
+function sendComAll(command)
+    monitor.setCursorPos(1,1)
+    monitor.write("Current status of mining process")
+    monitor.setCursorPos(1,2)
+    monitor.write("--------------------------------")
+    for x=1,#turtles,1 do --Loop for all turtles
+        monitor.setCursorPos(1,x+3)
+        monitor.write("1 - State: " .. sendCom(turtles[x], command)) --Prints results from sendCom
+    end
+end
+
+--Commandlist:
+--DIG, FOR, LEF, DUN, DWN, RIG, UUP, DUP.
+--Modify for own use.
+--Modify turtlemine.lua to add new commands.
+
+sendComAll("DIG")
+sendComAll("FOR")
+sendComAll("LEF")
+for x=1,w/2,1 do
+    sendComAll("DIG")
+    sendComAll("FOR")
+end
+sendComAll("RIG")
+for x=1,turtleAmount,1 do
+    sendComAll("DUN")
+    sendComAll("DWN")
+end
+for y=1,h,1 do
+    for x=1,w/2,1 do
+        for z=1,d-1,1 do
+            sendComAll("DIG")
+            sendComAll("FOR")
+        end
+        sendComAll("RIG")
+        sendComAll("DIG")
+        sendComAll("FOR")
+        sendComAll("RIG")
+        for z=1,d-1,1 do
+            sendComAll("DIG")
+            sendComAll("FOR")
+        end
+
+        sendComAll("LEF")
+        if not (x>=w/2) then
+            sendComAll("DIG")
+            sendComAll("FOR")
+            sendComAll("LEF")
+        end
+    end
+    if not (y>=tonumber(h)) then
+        for x=1,turtleAmount,1 do
+            sendComAll("DUP")
+            sendComAll("UUP")
+        end
+        sendComAll("LEF")
+        sendComAll("LEF")
+        sendComAll("DIG")
+        for x=1,w-1,1 do
+            sendComAll("DIG")
+            sendComAll("FOR")
+        end
+        sendComAll("RIG")
+    end
+end
+
+for x=1,turtleAmount*h,1 do
+    sendComAll("DWN")
+end
+
+monitor.clear()
+monitor.setCursorPos(1,1)
+monitor.write("Mining finished!")
+monitor.setCursorPos(1,2)
+monitor.write("----------------")
+sleep(2)
+
+os.reboot()
---
+++ turtlemine.lua
@@ -0,0 +1,51 @@
+rednet = peripheral.find("modem", rednet.open)
+local computerID = 4 --Modify to your computer's ID
+
+while 1 do
+    --Execute computer's commands through rednet
+    print("works " .. os.getComputerID())
+    local id, message = rednet.receive()
+    if id==computerID then
+        sleep(0.5)
+        print("works2")
+        --Commandlist:
+        --DIG, FOR, LEF, DUN, DWN, RIG, UUP.
+        --Modify for own use.
+        --Modify turtlemine.lua to add new commands.
+        if message=="This is computer " .. computerID then
+            rednet.send(computerID, "This is turtle " .. os.getComputerID())
+        end
+        if message=="DIG" then
+            rednet.send(computerID, "Digging " .. "F: " .. turtle.getFuelLevel())
+            --turtle.dig()
+        end
+        if message=="FOR" then
+            rednet.send(computerID, "Moving forward " .. "F: " .. turtle.getFuelLevel())
+            --turtle.forward()
+        end
+        if message=="LEF" then
+            rednet.send(computerID, "Turning left " .. "F: " .. turtle.getFuelLevel())
+            --turtle.turnLeft()
+        end
+        if message=="DUN" then
+            rednet.send(computerID, "Digging down " .. "F: " .. turtle.getFuelLevel())
+            --turtle.digDown()
+        end
+        if message=="DWN" then
+            rednet.send(computerID, "Moving down " .. "F: " .. turtle.getFuelLevel())
+            --turtle.down()
+        end
+        if message=="RIG" then
+            rednet.send(computerID, "Turning right " .. "F: " .. turtle.getFuelLevel())
+            --turtle.turnRight()
+        end
+        if message=="UUP" then
+            rednet.send(computerID, "Moving up " .. "F: " .. turtle.getFuelLevel())
+            --turtle.up()
+        end
+        if message=="DUP" then
+            rednet.send(computerID, "Moving up " .. "F: " .. turtle.getFuelLevel())
+            --turtle.digUp()
+        end
+    end
+end
